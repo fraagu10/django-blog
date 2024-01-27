7db94494adc8acf51ddafedd088a369f9f72e38f
fraagu10 <aguilarfranco1092@gmail.com>
2024-01-27 11:31:11 -0600

    added blog application to site with basic database access

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ka#bda1-vwf!)18o4n7)%#3i*$bdji(5djs_nx!tf#12*g#(#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
